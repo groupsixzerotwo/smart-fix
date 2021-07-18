ec28f10dff37b751d3a3967091aec17a3190d9b9
Abhishek Jamwal <jamwal.ab@gmail.com>
2021-07-18 19:48:14 -0400

    User model created

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//-----MIDDLEWARE-----//
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//-----CONNECT TO THE DATABASE-----//
 sequelize.sync({force: true}).then(() => {
   app.listen(PORT, () => console.log('Now istening'));
 })
