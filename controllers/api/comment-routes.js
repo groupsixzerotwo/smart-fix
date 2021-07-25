cadf8fa1f1a2521cc598842a3d849347f158b551
Rabeea Jahangir <rabeeajahangir@Rabeeas-MacBook-Air.local>
2021-07-24 23:28:27 -0400

    add comments

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -20,4 +20,22 @@ router.get('/', (req, res) => {
   });
 });
 
+router.post('/', (req, res) => {
+  // check the session
+  if (req.session) {
+    Comment.create({
+      comment_text: req.body.comment_text,
+      post_id: req.body.post_id,
+      // use the id from the session
+      user_id: req.session.user_id
+    })
+      .then(dbCommentData => res.json(dbCommentData))
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  }
+});
+
+
 module.exports = router;
--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -62,4 +62,17 @@ router.get('/:id', (req, res) => {
   });
 });
 
+router.put('/upvote', (req, res) => {
+  // make sure the session exists first
+  if (req.session) {
+    // pass session id along with all destructured properties on req.body
+    Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+      .then(updatedVoteData => res.json(updatedVoteData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  }
+});
+
 module.exports = router;
