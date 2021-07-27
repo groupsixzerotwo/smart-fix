33aad81ab4dbc28fea533f3bc835e527286b823f
Abhishek Jamwal <jamwal.ab@gmail.com>
2021-07-27 04:40:51 -0400

    login signup fixed

--- public/assets/javascript/signup.js
+++ public/assets/javascript/signup.js
@@ -4,12 +4,14 @@ async function signupFormHandler(event) {
   const username = document.querySelector('#username-signup').value.trim();
   const email = document.querySelector('#email-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
-  const sp = document.querySelector("input[name=service]:checked").value;
+  const sp = document.querySelector("input[name=serviceYesNo]:checked").value;
   let service_id = null;
   if (sp === "Yes") {
-    const service = document.querySelector("select[name=service-type]");//.value.trim();
-    service_id = service.options[service.selectedIndex].value
-  }
+    const service = document.querySelector("input[name=service-list]:checked");
+    service_id = service.value;//options[service.selectedIndex].value
+  };
+
+  console.log(username, email, password, sp, service_id)
 
   if (username && email && password && sp) {
     const response = await fetch('/api/users', {
@@ -32,4 +34,14 @@ async function signupFormHandler(event) {
   }
 }
 
+function serviceShow() {
+  document.querySelector(".service-type").style.display = "block";
+}
+
+function serviceHide() {
+  document.querySelector(".service-type").style.display = "none";
+}
+
+document.querySelector('#service-yes').addEventListener('click', serviceShow);
+document.querySelector('#service-no').addEventListener('click', serviceHide);
 document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
