18ae8059e5f4a49a5b02a75803288d8eb4534204
Abhishek Jamwal <jamwal.ab@gmail.com>
2021-07-28 04:29:41 -0400

    Single post and job html route added"

--- controllers/html/jobOrders-routes.js
+++ controllers/html/jobOrders-routes.js
@@ -4,6 +4,7 @@ const { User, Service, Assignment, Job, Status, Rating } = require('../../models
 
 router.get('/', (req, res) => {
   const theuser = req.session.username;
+  const isService = req.session.service_id;
   //const service = req.session.service_id;
   //----GET ASSIGNMENT AND JOB DATA FOR SERVICE PROVIDER----//
   if (req.session.service_id) {
@@ -59,7 +60,6 @@ router.get('/', (req, res) => {
       .then(([assignData, jData]) => {
       const assignmentData = assignData.map(assignment => assignment.get({plain: true}));
       const jobData = jData.map(assignment => assignment.get({plain: true}));
-      console.log(assignmentData, jobData);
       res.render('jobOrdersSP', {assignmentData, jobData, theuser, loggedIn: req.session.loggedIn})
     });    
   } 
@@ -92,6 +92,94 @@ router.get('/', (req, res) => {
       res.render('jobOrdersC', {jobData, theuser, loggedIn: req.session.loggedIn})
     });    
   }
-})
+});
+
+router.get('/assignment/:id', (req, res) => {
+  Assignment.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: User,
+        attributes: ['id', 'username'],
+        include: {
+          model: Service
+        }
+      },
+      {
+        model: Job,
+        include: [
+          {
+            model: Status
+          },
+          {
+            model: Rating,
+            attributes: ['id', 'points']
+          },
+          {
+            model: User,
+            attributes: ['id', 'username', 'email']
+          }
+        ]
+      }
+    ]
+  })
+  .then(dbAssignmentData => {
+    if(!dbAssignmentData) {
+      res.status(404).json({message: 'No service found with this id'});
+      return;
+    }
+    res.render('jobOrdersC', {dbAssignmentData, theuser, isService, loggedIn: req.session.loggedIn})
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err)
+  });
+});
+
+router.get('/job/:id', (req, res) => {
+  Job.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Status
+      },
+      {
+        model: User,
+        //User customer
+        attributes: ['id', 'username', 'email'],
+      },
+      {
+        model: Assignment,
+        include: {
+          //User service provider
+          model: User,
+          attributes: ['id', 'username'],
+          include: {
+            model: Service
+          }
+        }
+      },
+      {
+        model: Rating,
+        attributes: ['id', 'points']
+      }
+    ]
+  })
+  .then(dbJobData => {
+    if(!dbJobData) {
+      res.status(404).json({message: 'No job found with this id'});
+      return;
+    }
+    res.render('jobOrdersC', {dbJobData, theuser, isService, loggedIn: req.session.loggedIn})
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
 
 module.exports = router;
