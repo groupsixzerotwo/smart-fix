ff4fd8f333333f0453344a2bf7e7f856d6abc73b
Sona <sonali23.1293@gmail.com>
2021-07-30 19:34:06 -0400

    login page responsive

--- public/assets/javascript/login.js
+++ public/assets/javascript/login.js
@@ -7,8 +7,8 @@ async function signupFormHandler(event) {
 async function loginFormHandler(event) {
   event.preventDefault();
 
-  const email = document.querySelector('#email-login').value.trim();
-  const password = document.querySelector('#password-login').value.trim();
+  const email = document.querySelector('.email-login').value.trim();
+  const password = document.querySelector('.password-login').value.trim();
 
   if (email && password) {
     const response = await fetch('/api/users/login', {
